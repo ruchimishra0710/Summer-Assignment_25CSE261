80af6dec095163af2b62e982504d66c7f4172f79
ruchimishra0710 <ruchimishra0710@gmail.com>
2026-06-27 22:16:43 +0530

    Day 8

---
+++ Q31.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (char ch = 'A'; ch < 'A' + i; ch++)
+        {
+            cout << ch;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
